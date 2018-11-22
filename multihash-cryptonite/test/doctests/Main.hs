a0f61b1880ff2fab7f51611c3af3884b9b0145e3
Kim Altintop <kim@monadic.xyz>
2018-11-22 17:03:16 +0100

    stylish-haskell

--- multihash-cryptonite/test/doctests/Main.hs
+++ multihash-cryptonite/test/doctests/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
-import System.FilePath.Glob (glob)
-import Test.DocTest
+import           System.FilePath.Glob (glob)
+import           Test.DocTest
 
 main :: IO ()
 main = do
